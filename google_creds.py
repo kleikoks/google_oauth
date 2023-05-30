e35f0ce52256568da06cd1b70bd1b3fcd6409dc3
kleikoks <kleikoks.py@gmail.com>
2023-05-30 10:24:48 +0300

    remove unused code

--- google_creds.py
+++ google_creds.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import os.path
 
 from google.auth.transport.requests import Request
@@ -40,5 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
